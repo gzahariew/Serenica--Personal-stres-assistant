80f567d6bb422f6f28a22586060d9fde9f1495a4
Jorkata Zah <gzaharievwork@gmail.com>
2025-06-24 18:36:23 +0300

    Added controllers as well as a user service file taking care of the user logic

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import { errorHandler } from './middleware/errHandler.js'; // Import the error handler
- 
-
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler } from "./middleware/errHandler.js"; // Import the error handler
 
 dotenv.config();
 const app = express();
@@ -13,11 +11,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 // Routes
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 
 // Error handling middleware
